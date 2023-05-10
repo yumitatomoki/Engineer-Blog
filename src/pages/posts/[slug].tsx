1f52c4082fd3d179cbc3c075b72228840a85d281
yumitaito <tomokiyumita0605@gmail.com>
2023-05-10 12:31:10 +0900

    ✨ ブログの詳細ページを作成

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,7 +1,43 @@
-const PostDetailPage = () => {
-  return (
-      <div>PostDetailPage</div>
-  )
+import { Box,Container,Heading,Text, Center} from '@chakra-ui/react'
+import { EntryCollection } from 'contentful';
+import {client} from '../../utils/contentfulClient'
+import { IMyPostsFields } from '../../../@types/generated/contentful'
+import { NextPage,GetServerSideProps, GetServerSidePropsResult} from "next";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import {options} from '../../../lib/richTextOption';
+
+type PostDetailsPageProps = {
+  blogPost: EntryCollection<IMyPostsFields>['items'][0]
 }
 
-export default PostDetailPage
+const PostDetailsPage: NextPage<PostDetailsPageProps> = ({blogPost}) => {
+
+  return (
+    <Container maxW={1024}>
+      <Center>
+        <Heading mr='auto'>{blogPost.fields.title}</Heading>
+        <Text>{(blogPost.sys.createdAt).substring(0, 10)}</Text>
+      </Center>
+      <Box>{documentToReactComponents(blogPost.fields.content,options)}</Box>
+    </Container>
+  )
+} 
+
+export default PostDetailsPage;
+
+export const getServerSideProps: GetServerSideProps<PostDetailsPageProps> = async ({ params }) : Promise<GetServerSidePropsResult<PostDetailsPageProps>> => {
+  const { items } = await client.getEntries<IMyPostsFields>({
+    content_type: 'blogPosts',
+    'fields.slug': (params?.slug ?? '').toString(),
+  });
+
+  if (!items.length) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { blogPost: items[0] },
+  };
+};
+
